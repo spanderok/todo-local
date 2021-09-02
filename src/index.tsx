9358f167a695a13fbfadb8524b74e2af6af203a2
Dzmitry Palyka <Palyka_DV@local.st.by>
2021-09-02 15:50:30 +0300

    feat: init project

---
+++ src/index.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { action, observable } from 'mobx';
+import { observer } from 'mobx-react';
+
+import './style.css';
+
+class AppState {
+  @observable timer = 0;
+
+  constructor() {
+    setInterval(() => {
+      this.timer += 1;
+    }, 1000);
+  }
+
+  @action
+  resetTimer() {
+    this.timer = 0;
+  }
+}
+
+const Timer = observer(() => {
+  const service = React.useMemo(() => new AppState(), []);
+
+  return (
+    <div>
+      <button
+        onClick={() => service.resetTimer()}
+        className={service.timer % 2 === 0 ? 'complete' : ''}
+      >
+        Seconds passed: {service.timer}
+      </button>
+    </div>
+  );
+});
+
+ReactDOM.render(<Timer />, document.getElementById('root'));
